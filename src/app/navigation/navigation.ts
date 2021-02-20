dc50ffa9375fa1848fd735e56685a9a2470ef55c
Muhammed Fuat Nuroğlu <fuat.nuroglu@dgpaysit.com>
2021-02-20 18:07:34 +0300

    indenting + ufak tefek

--- src/app/navigation/navigation.ts
+++ src/app/navigation/navigation.ts
@@ -2,24 +2,18 @@ import { FuseNavigation } from '@fuse/types';
 
 export const navigation: FuseNavigation[] = [
     {
-        id       : 'applications',
-        title    : 'Applications',
-        translate: 'NAV.APPLICATIONS',
-        type     : 'group',
-        children : [
+        id: 'botmanagement',
+        title: 'Bot Management',
+        translate: 'NAV.BOT_MANAGEMENT',
+        type: 'collapsable',
+        children: [
             {
-                id       : 'sample',
-                title    : 'Sample',
-                translate: 'NAV.SAMPLE.TITLE',
-                type     : 'item',
-                icon     : 'email',
-                url      : '/sample',
-                badge    : {
-                    title    : '25',
-                    translate: 'NAV.SAMPLE.BADGE',
-                    bg       : '#F44336',
-                    fg       : '#FFFFFF'
-                }
+                id: 'categories',
+                title: 'Categories',
+                translate: 'NAV.CATEGORIES',
+                type: 'item',
+                icon: 'fastfood',
+                url: '/sample'
             }
         ]
     }
