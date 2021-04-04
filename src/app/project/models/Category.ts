7a9a9a4f38e61f67ccda82b04de22033de275b0b
Muhammed Fuat Nuroğlu <fuat.nuroglu@dgpaysit.com>
2021-04-04 16:18:13 +0300

    Categories sayfası eklendi

---
+++ src/app/project/models/Category.ts
@@ -0,0 +1,6 @@
+export interface Category {
+    Id: number;
+    Name: string;
+    CategoryKey: string;
+
+}
---
+++ src/app/project/pages/bot-management/categories/categories.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CategoriesRoutingModule } from './categories-routing.module';
+import { CategoriesComponent } from './categories.component';
+import { FuseSharedModule } from '@fuse/shared.module';
+import { GetService } from 'app/project/services/get.service';
+
+
+@NgModule({
+  declarations: [CategoriesComponent],
+  imports: [
+    FuseSharedModule,
+    CategoriesRoutingModule
+  ],
+  providers: [GetService]
+})
+export class CategoriesModule { }
